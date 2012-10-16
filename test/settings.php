018875bae27e68424d27f759f9fef0f4ce6ffce0
hit9 <nz2324@126.com>
2012-10-17 00:37:55 +0800

    ignore

--- test/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST','localhost');
-define('DB_NAME','mydb');
-define('DB_USER','root');
-define('DB_PASSWD','123456');
-define('TABLE_PREFIX','t_');
-define('DB_CHAR_SET','utf8');
-define('QUERY_ERROR',true);
